26fea26d1e97932d7de4b93bda8d6fbe8fe33126
Divine <dpkreativ@gmail.com>
2022-04-26 09:40:38 +0100

    Create components

---
+++ components/CreateAlbumArt.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import { LoadingSVG } from './Icons';
+
+const CreateAlbumArt = ({
+  textChange,
+  fileChange,
+  onSubmit,
+  textValue,
+  loading,
+}) => {
+  return (
+    <div>
+      <form
+        action="POST"
+        onSubmit={onSubmit}
+        className="flex flex-col space-y-2 items-center"
+      >
+        <div className="flex space-x-2">
+          <div className="relative">
+            <input
+              type="file"
+              name="file"
+              className="absolute z-10 w-32 h-12 opacity-0"
+              onChange={fileChange}
+            />
+            <label
+              htmlFor="file"
+              className="bg-blue-500 text-white rounded-lg shadow-md p-3 block relative w-32 h-12 text-center"
+            >
+              Select image
+            </label>
+          </div>
+          <input
+            type="text"
+            value={textValue}
+            onChange={textChange}
+            className="px-3 border-2 border-blue-400 rounded shadow-lg outline-none"
+            placeholder="Add album text here"
+          />
+        </div>
+        <button
+          type="submit"
+          className="bg-green-500 hover:bg-green-600 transition-all text-white font-semibold py-2 px-4 h-12 shadow rounded-lg w-max"
+        >
+          {loading ? (
+            <span className="flex items-center space-x-2">
+              <LoadingSVG />
+              <p>Creating...</p>
+            </span>
+          ) : (
+            'Create your album art'
+          )}
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default CreateAlbumArt;
---
+++ components/UploadPopArt.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { LoadingSVG } from './Icons';
+
+const UploadPopArt = ({ onChange, onSubmit, loading }) => {
+  return (
+    <div>
+      <form
+        action="POST"
+        onChange={onChange}
+        onSubmit={onSubmit}
+        className="flex flex-col items-center space-y-2"
+      >
+        <div className="relative">
+          <input
+            type="file"
+            name="file"
+            className="absolute z-10 w-52 h-12 opacity-0"
+            onChange={onChange}
+          />
+          <label
+            htmlFor="file"
+            className="bg-blue-500 text-white rounded-lg shadow-md p-3 block relative w-52 h-12 text-center"
+          >
+            Select Popular Album Art
+          </label>
+        </div>
+        <button
+          type="submit"
+          className="bg-green-500 hover:bg-green-600 transition-all text-white font-semibold h-12 py-2 px-4 shadow rounded-lg w-max"
+        >
+          {loading ? (
+            <span className="flex items-center space-x-2">
+              <LoadingSVG />
+              <p>Applying...</p>
+            </span>
+          ) : (
+            'Apply Neural Style Transfer'
+          )}
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default UploadPopArt;
